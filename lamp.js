8aed1d3cf6ef3d15384b15440a2e15cfa0a75e07
helo lolo <143643834+helooli@users.noreply.github.com>
2024-11-14 09:35:48 -0300

    Add files via upload

--- lamp.js
+++ lamp.js
@@ -3,10 +3,28 @@ var $desligar = document.getElementById('desligar')
 var $lampada = document.getElementById('lamp')
 
 
-
 function ligarLamp () {
-
+ 
     $lampada.src = 'imagens/lampada-acesa.jpg'
 }
 
+function desligarLamp(){
+
+    $lampada.src = 'imagens/lampada-apagada.jpg'
+}
+
+function quebrarLamp(){
+
+    $lampada.scr = 'imagens/lampada-quebrada.jpg'
+
+}
+
 $ligar.addEventListener('click' ,ligarLamp)
+
+$desligar.addEventListener('click', desligarLamp)
+
+$lampada.addEventListener('mouseover', ligarLamp)
+
+$lampada.addEventListener('mouseleave', desligarLamp)
+
+$lampada.addEventListener('dblclick',quebrarLamp);
